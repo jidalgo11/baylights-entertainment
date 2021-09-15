d8a9726519decb2c3881512e0681ccd80b172b79
Daniel Hidalgo <danielhidalgo@Daniels-MacBook-Pro-3.local>
2021-09-15 04:47:53 -0700

    added nav functionality and about us section

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
 const header = document.querySelector('header')
 const hero = document.querySelector('.hero')
 
-// $(window).scroll(function () {
-//   var intro = $('.intro').innerHeight();
-//   if ($(this).scrollTop() > intro + 50) {
-//     $('header').addClass('sticky');
-//   } else {
-//     $('header').removeClass('sticky');
-//   }
-// });
+$(function () {
+  $('a[href*="#"]:not([href="#"])').click(function () {
+    $('nav').removeClass('active');
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 100
+        }, 750);
+        return false;
+      }
+    }
+  });
+});
+
+$('#top').click(function (e) {
+  e.preventDefault();
+  $('html, body').animate({
+    scrollTop: 0
+  }, 750);
+});
 
 window.addEventListener('scroll', () => {
   if(window.scrollY > 100) {
